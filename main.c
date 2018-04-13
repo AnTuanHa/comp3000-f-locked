45b095c0df0ac0b548c59afb282287d0b922abf0
An Ha <zero579911@gmail.com>
2018-04-12 20:39:15 -0400

    Return useful error codes on program exit

--- main.c
+++ main.c
@@ -14,6 +14,15 @@
 #define KEY_PLAINTEXT "plaintext"
 #define KEY_PERM "permissions"
 
+#define ESTDERR -1
+#define EWRONG_PASSWORD -2
+#define EFILE_NOT_EXISTS -3
+#define EFILE_NOT_OWNED_BY_USER -4
+#define EFILE_CHANGE_PERMISSIONS -5
+#define EFILE_SET_XATTR -6
+#define EFILE_GET_XATTR -7
+#define EFILE_RM_XATTR -8
+
 int lock_file(const char *path);
 int unlock_file(const char *path);
 
@@ -21,36 +30,36 @@ int main(int argc, char *argv[])
 {
     if (argc <= 1) {
         printf("usage: %s PATH-TO-FILE\n", argv[0]);
-        return -1;
+        return ESTDERR;
     }
 
     if (access(argv[1], F_OK) < 0) {
         printf("File '%s' does not exist\n", argv[1]);
-        return -1;
+        return EFILE_NOT_EXISTS;
     }
 
     // Ensure that the file the user is locking is owned by the user
     if (!check_group(argv[1])) {
         printf("File '%s' is not owned by current user!\n", argv[1]);
         printf("Aborting file locking\n");
-        return -1;
+        return EFILE_NOT_OWNED_BY_USER;
     }
+
+    int ret = 0;
 
     // Lock does not exist
     if (!attr_exists(argv[1], SECURITY_CIPHERTEXT) &&
             !attr_exists(argv[1], SECURITY_PLAINTEXT)) {
-        if (lock_file(argv[1])) {
+        ret = lock_file(argv[1]);
+        if (ret)
             printf("Failed to lock file '%s'\n", argv[1]);
-            return -1;
-        }
     } else {
-        if (unlock_file(argv[1])) {
+        ret = unlock_file(argv[1]);
+        if (ret)
             printf("Failed to unlock file '%s'\n", argv[1]);
-            return -1;
-        }
     }
 
-    return 0;
+    return ret;
 }
 
 int lock_file(const char *path)
@@ -73,20 +82,20 @@ int lock_file(const char *path)
 
     if (set_perm(path, 0000) < 0) {
         printf("Failed to change permissions on %s\n", path);
-        return -1;
+        return EFILE_CHANGE_PERMISSIONS;
     }
 
     if (setattr(path, KEY_CIPHERTEXT, cipher) < 0) {
         printf("Failed to set extended attribute '%s' on %s\n", SECURITY_CIPHERTEXT, path);
-        return -1;
+        return EFILE_SET_XATTR;
     }
     if (setattr(path, KEY_PLAINTEXT, plain) < 0) {
         printf("Failed to set extended attribute '%s' on %s\n", SECURITY_PLAINTEXT, path);
-        return -1;
+        return EFILE_SET_XATTR;
     }
     if (setattr(path, KEY_PERM, perms) < 0) {
         printf("Failed to set extended attribute '%s' on %s\n", SECURITY_PERM, path);
-        return -1;
+        return EFILE_SET_XATTR;
     }
 
     printf("File successfully locked with the given password\n");
@@ -105,14 +114,14 @@ int unlock_file(const char *path)
     char cipher[MAX_SIZE];
     if (getxattr(path, SECURITY_CIPHERTEXT, cipher, MAX_SIZE - 1) < 0) {
         printf("Failed to get extended attribute '%s' on %s\n", SECURITY_CIPHERTEXT, path);
-        return -1;
+        return EFILE_GET_XATTR;
     }
     cipher_block = string_to_blocks(cipher);
 
     char plain[MAX_SIZE];
     if (getxattr(path, SECURITY_PLAINTEXT, plain, MAX_SIZE - 1) < 0) {
         printf("Failed to get extended attribute '%s' on %s\n", SECURITY_PLAINTEXT, path);
-        return -1;
+        return EFILE_GET_XATTR;
     }
     plain_block = string_to_blocks(plain);
 
@@ -129,31 +138,32 @@ int unlock_file(const char *path)
         char perm[MAX_SIZE];
         if (getxattr(path, SECURITY_PERM, perm, MAX_SIZE - 1) < 0) {
             printf("Failed to get extended attribute '%s' on %s\n", SECURITY_PERM, path);
-            return -1;
+            return EFILE_GET_XATTR;
         }
         int p = (int)strtol(perm, (char **)NULL, 10);
 
         if (removexattr(path, SECURITY_CIPHERTEXT) < 0) {
             printf("Failed to remove extended attribute '%s' on %s\n", SECURITY_CIPHERTEXT, path);
-            return -1;
+            return EFILE_RM_XATTR;
         }
         if (removexattr(path, SECURITY_PLAINTEXT) < 0) {
             printf("Failed to remove extended attribute '%s' on %s\n", SECURITY_PLAINTEXT, path);
-            return -1;
+            return EFILE_RM_XATTR;
         }
         if (removexattr(path, SECURITY_PERM) < 0) {
             printf("Failed to remove extended attribute '%s' on %s\n", SECURITY_PERM, path);
-            return -1;
+            return EFILE_RM_XATTR;
         }
 
         if (set_perm(path, p) < 0) {
             printf("Failed to change permissions on %s\n", path);
-            return -1;
+            return EFILE_CHANGE_PERMISSIONS;
         }
 
         printf("Success\n");
     } else {
         printf("Wrong password\n");
+        return EWRONG_PASSWORD;
     }
 
     free(plain_block);
